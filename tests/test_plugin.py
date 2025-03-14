dc1fba50ac17ba074e0ae35fa559701c17cd197d
Artur Balabanov <arturbalabanov97@gmail.com>
2025-03-14 13:10:03 +0000

    feat: Fix warnings caused by pytest-asyncio

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -1,4 +1,17 @@
+import pytest
 from pytest import Pytester
+
+
+@pytest.fixture(autouse=True)
+def setup_asyncio_default_fixture_loop_scope(pytester: Pytester) -> None:
+    # This option is set on this project but the tests here are for a brand new one
+    # so we need to set it up here too. This is necessary to suppress a warning
+    # by pytest-asyncio about the default loop scope.
+
+    pytester.makeini("""
+        [pytest]
+        asyncio_default_fixture_loop_scope = "session"
+    """)
 
 
 def test_plugin_without_db_engine_fixture(pytester: Pytester) -> None:
@@ -7,7 +20,7 @@ def test_plugin_without_db_engine_fixture(pytester: Pytester) -> None:
             pass
     """)
 
-    result = pytester.runpytest("--asyncio-mode=strict")
+    result = pytester.runpytest()
 
     result.assert_outcomes(errors=1)
     assert "fixture 'db_engine' not found" in result.stdout.str()
