ced727906ed4fa639247f433f20f6a087623d824
Artur Balabanov <arturbalabanov97@gmail.com>
2025-03-19 10:46:05 +0000

    feat: Complete the documentation of the plugin (MPT-8012)

--- pytest_capsqlalchemy/plugin.py
+++ pytest_capsqlalchemy/plugin.py
@@ -14,5 +14,21 @@ def capsqlalchemy_context(db_engine: AsyncEngine) -> Generator[SQLAlchemyCapture
 
 @pytest.fixture()
 def capsqlalchemy(capsqlalchemy_context: SQLAlchemyCaptureContext) -> SQLAlchemyCapturer:
-    """The main fixture!"""
+    """The main fixture to get the [`SQLAlchemyCapturer`][pytest_capsqlalchemy.capturer.SQLAlchemyCapturer].
+
+    Example Usage:
+
+    ```python
+    async def test_some_sql_queries(db_session, capsqlalchemy):
+        await db_session.execute(select(text("1")))
+        capsqlalchemy.assert_query_count(1, include_tcl=False)
+
+        async with capsqlalchemy:
+            await db_session.execute(select(text("2")))
+            await db_session.execute(select(text("3")))
+            capsqlalchemy.assert_query_count(2, include_tcl=False)
+
+        capsqlalchemy.assert_query_count(3, include_tcl=False)
+    ```
+    """
     return SQLAlchemyCapturer(capsqlalchemy_context)
